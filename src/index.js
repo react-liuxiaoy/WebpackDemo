3834b82fe5afa00024bc1848dace514f87f638cd
liuxy <938651469@qq.com>
2018-08-01 10:32:26 +0800

    loading image file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './index.css';
+import Icon from './logo.svg';
 
 function component() {
     var element = document.createElement('div');
@@ -8,6 +9,11 @@ function component() {
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
 
+    // Add the image to our existing div.
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
     return element;
 }
 
